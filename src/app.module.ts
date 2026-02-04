f33e15d1a5453238f2cc610269f25da9550edaca
Ronal S. <ronal.sanx21@gmail.com>
2026-02-04 14:25:47 -0500

    Manejo de imagenes

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,13 @@ import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 
 @Module({
   imports: [
+    
     ConfigModule.forRoot({}),
     TypeOrmModule.forRoot({
       type: 'postgres',
@@ -19,14 +22,15 @@ import { FilesModule } from './files/files.module';
       password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
       synchronize: true
-
     }),
+
     ProductsModule,
     CommonModule,
     SeedModule,
-    FilesModule
+    FilesModule,
+
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
 
 
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Main Server')
   app.setGlobalPrefix('api/');
   app.useGlobalPipes(
     new ValidationPipe({
@@ -15,6 +16,6 @@ async function bootstrap() {
   )
 
   await app.listen(process.env.PORT ?? 3000);
-
+  logger.log(`App Running on PORT ${process.env.PORT ?? 3000}`)
 }
 bootstrap();
